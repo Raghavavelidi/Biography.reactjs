498290caa00ba3f0ef894654b5e942302351f3ae
Raghavavelidi <135940607+Raghavavelidi@users.noreply.github.com>
2023-08-11 15:13:22 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,23 @@
+ReactDOM.render(
+    <div>
+        <h1>Che Guevara</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/CheHigh.jpg/1200px-CheHigh.jpg" height="600px" ></img>
+        <h3>Born :Ernesto Guevara
+14 June 1928 Rosario, Santa Fe, Argentina </h3>
+        <h3> 9 October 1967</h3>
+        <h3>Minister of Industries of Cuba</h3>	
+         
+        <h3>AWARDS</h3>
+        <ol>
+            <li>Copley Medal (1794)</li>
+            <li>Legion of Honour</li>
+            <li>Order of the Iron Crown</li>
+        </ol>
+         
+        <p> As a young medical student, Guevara traveled throughout South America and was radicalized by the poverty, hunger, and disease he witnessed.[6][7] His burgeoning desire to help overturn what he saw as the capitalist exploitation of Latin America by the United States prompted his involvement in Guatemala's social reforms under President Jacobo Árbenz, whose eventual CIA-assisted overthrow at the behest of the United Fruit Company solidified Guevara's political ideology.[6] Later in Mexico City, Guevara met Raúl and Fidel Castro, joined their 26th of July Movement, and sailed to Cuba aboard the yacht Granma with the intention of overthrowing U.S.-backed dictator Fulgencio Batista.[8] Guevara soon rose to prominence among the insurgents, was promoted to second-in-command, and played a pivotal role in the two-year guerrilla campaign that deposed the Batista regime.[9]
+
+After the Cuban Revolution, Guevara played key roles in the new government. These included reviewing the appeals and firing squads for those convicted as war criminals during the revolutionary tribunals,[10] instituting agrarian land reform as Minister of Industries, helping spearhead a successful nationwide literacy campaign, serving as both President of the National Bank and instructional director for Cuba's armed forces, and traversing the globe as a diplomat on behalf of Cuban socialism. Such positions also allowed him to play a central role in training the militia forces who repelled the Bay of Pigs Invasion,[11] and bringing Soviet nuclear-armed ballistic missiles to Cuba, which preceded the 1962 Cuban Missile Crisis.[12] Additionally, Guevara was a prolific writer and diarist, composing a seminal guerrilla warfare manual, along with a best-selling memoir about his youthful continental motorcycle journey. His experiences and studying of Marxism–Leninism led him to posit that the Third World's underdevelopment and dependence was an intrinsic result of imperialism, neocolonialism, and monopoly capitalism, with the only remedies being proletarian internationalism and world revolution</p> 
+    <p>In 1809, Volta became associated member of the Royal Institute of the Netherlands. In honour of his work, Volta was made a count by Napoleon Bonaparte in 1810.
+Volta retired in 1819 to his estate in Camnago, a frazione of Como, Italy, now named "Camnago Volta" in his honour. He died there on 5 March 1827, just after his 82nd birthday.Volta's remains were buried in Camnago Volta.</p>
+    </div>,document.getElementById("biography")
+);
